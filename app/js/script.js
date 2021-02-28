4c678bcd038783c1545693c9d289644ffaacb2c0
Chad James <robertsonchad1@gmail.com>
2021-02-28 13:39:56 -0600

    actual initial commit

---
+++ app/js/script.js
@@ -0,0 +1,26 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+
+btnHamburger.addEventListener('click', function () {
+	console.log('open hamburger');
+	if (header.classList.contains('open')) {
+		// close hamburger menu
+		body.classList.remove('noScroll');
+		header.classList.remove('open');
+		fadeElems.forEach(function (element) {
+			element.classList.remove('fade-in');
+			element.classList.add('fade-out');
+		});
+	} else {
+		// open hamburger menu
+		body.classList.add('noScroll');
+		header.classList.add('open');
+		fadeElems.forEach(function (element) {
+			element.classList.remove('fade-out');
+			element.classList.add('fade-in');
+		});
+	}
+});
